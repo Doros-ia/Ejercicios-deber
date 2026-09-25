b698b527a8e7411bf9257f9e0136657c4e429e69
Karina-com <c51032056@gmail.com>
2026-09-25 10:05:12 -0500

    Add files via upload

---
+++ Ejercicio 8/DetectorCorregido.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class DetectorCorregido {
+    public static void main(String[] args) {
+        int numero = 1;
+
+        while (numero <= 10) {
+            System.out.println(numero);
+            numero++;
+        }
+    }
+}
